044a66462e415587bd67b72385eb823934cfdfce
Sky Swimmer <85841243+SkySwimmer@users.noreply.github.com>
2023-02-02 10:55:45 +0100

    Initial commit

---
+++ src/patcher/Patcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Mono.Cecil;
+using BepInEx.Logging;
+
+namespace ExamplePatcher
+{
+	public static class Patcher
+	{
+		// List of assemblies to patch
+		public static IEnumerable<string> TargetDLLs { get; } = new[] {"Assembly-CSharp.dll"};
+
+		// Patches the assemblies
+		public static void Patch(AssemblyDefinition assembly)
+		{
+			// Patcher code here
+
+			// BEWARE: patchers CANNOT be debugged using the unity tools as they load too early for that
+			// You will need to use logging for debugging the patcher
+
+			// Its highly reommended to make it a habit to include A LOT of debug logging using Logger.LogDebug
+			// You will need to configure BepInEx to show debug log
+
+			// Create logger
+			ManualLogSource log = Logger.CreateLogSource("Patcher");
+			log.LogInfo("Loaded the example patcher for assembly: " + assembly.FullName);
+		}
+	}
+}
